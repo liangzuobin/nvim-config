986c94cac424067ff39a6c715f9aa0aa0b2b944c
liangzuobin <liangzuobin@kingsoft.com>
2024-02-09 22:33:59 +0800

    coc

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -257,13 +257,13 @@ require("lazy").setup({
     "rcarriga/nvim-dap-ui",
     dependencies = { { 'mfussenegger/nvim-dap' } },
   },
-  {
-    "vim-airline/vim-airline",
-    lazy = false,
-    priority = 1000,
-    dependencies = {
-      { "vim-airline/vim-airline-themes" },
-      { "ryanoasis/vim-devicons" },
-    }
-  }
+  -- {
+  --   "vim-airline/vim-airline",
+  --   lazy = false,
+  --   priority = 1000,
+  --   dependencies = {
+  --     { "vim-airline/vim-airline-themes" },
+  --     { "ryanoasis/vim-devicons" },
+  --   }
+  -- }
 })
